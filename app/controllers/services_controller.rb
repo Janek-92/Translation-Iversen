8bf0938a824d59540f9893ce6508c8a8f8f7d504
Janek Iversen <janekiversen@gmail.com>
2022-06-27 13:40:43 +0200

    forms new and services controller

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,33 @@
+class ServicesController < ApplicationController
+
+
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @services = Service.where("title ILIKE ?", "%#{params[:query]}%")
+    else
+      @services = Service.all
+    end
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    @service.user = current_user
+    @service.save!
+    redirect_to services_path(@service)
+  end
+
+private
+
+  def service_params
+    params.require(:service).permit(:title, :category, :price, :description, :availability)
+  end
+end
